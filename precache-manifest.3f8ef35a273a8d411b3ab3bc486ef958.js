476b0e4b284f5cb0e909752f0066719b7c91de01
Linxuan-Chen <moshouclxclx@163.com>
2020-09-22 20:48:29 -0400

    Updates

---
+++ precache-manifest.3f8ef35a273a8d411b3ab3bc486ef958.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7400eabdc3a7512f3b76fb7e1f98e8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8aeaa1606785eccbf24a",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "bc10ba7886a50c8f45f6",
+    "url": "/static/css/main.7d9335f9.chunk.css"
+  },
+  {
+    "revision": "8aeaa1606785eccbf24a",
+    "url": "/static/js/2.f96194c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f96194c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc10ba7886a50c8f45f6",
+    "url": "/static/js/main.b66f2f2a.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  },
+  {
+    "revision": "cdb0678eca16cf75d5e03dd5e592a39a",
+    "url": "/static/media/Island.cdb0678e.jpg"
+  }
+]);
