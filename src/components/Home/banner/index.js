d7d5996e96def113428e0afd77fb024582dfe919
Linxuan-Chen <moshouclxclx@163.com>
2020-09-22 20:57:39 -0400

    gh page test

--- src/components/Home/banner/index.js
+++ src/components/Home/banner/index.js
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2020-09-22 19:00:57
- * @LastEditTime: 2020-09-22 20:38:00
- * @LastEditors: your name
+ * @LastEditTime: 2020-09-22 20:54:39
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \repository\My_React_Website\src\components\Home\banner\index.js
  */
@@ -29,16 +29,16 @@ export default function Home() {
           Welcome To My Website
         </h2>
         <div className={style.clouds}>
-          <img src="/assets/banner/cloud1.png" alt="" />
-          <img src="/assets/banner/cloud2.png" alt="" />
-          <img src="/assets/banner/cloud3.png" alt="" />
-          <img src="/assets/banner/cloud4.png" alt="" />
-          <img src="/assets/banner/cloud5.png" alt="" />
-          <img src="/assets/banner/cloud1.png" alt="" />
-          <img src="/assets/banner/cloud2.png" alt="" />
-          <img src="/assets/banner/cloud3.png" alt="" />
-          <img src="/assets/banner/cloud4.png" alt="" />
-          <img src="/assets/banner/cloud5.png" alt="" />
+          <img src="assets/banner/cloud1.png" alt="" />
+          <img src="assets/banner/cloud2.png" alt="" />
+          <img src="assets/banner/cloud3.png" alt="" />
+          <img src="assets/banner/cloud4.png" alt="" />
+          <img src="assets/banner/cloud5.png" alt="" />
+          <img src="assets/banner/cloud1.png" alt="" />
+          <img src="assets/banner/cloud2.png" alt="" />
+          <img src="assets/banner/cloud3.png" alt="" />
+          <img src="assets/banner/cloud4.png" alt="" />
+          <img src="assets/banner/cloud5.png" alt="" />
         </div>
       </div>
     </div>
