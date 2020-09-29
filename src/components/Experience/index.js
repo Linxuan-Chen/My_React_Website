80fd53d7facb7a395a1d0a97ad38c1075c523e99
Linxuan-Chen <moshouclxclx@163.com>
2020-09-29 17:50:24 -0400

    meta info update

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -1,16 +1,15 @@
 /*
  * @Author: your name
  * @Date: 2020-09-23 16:10:35
- * @LastEditTime: 2020-09-28 14:37:07
+ * @LastEditTime: 2020-09-29 15:57:22
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \repository\My_React_Website\src\components\Experience\index.js
  */
 import React, { useEffect } from "react";
-import { NavLink } from "react-router-dom";
 import style from "./index.module.scss";
 
-function Experience(props) {
+function Experience() {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -21,81 +20,84 @@ function Experience(props) {
         <div className={style.mask}></div>
         <div className={style.wrapper + " container"}>
           <header className={style.header + " text-center"}>
-            <p className="pt-5">Experience</p>
+            <p className="pt-5">EXPERIENCE</p>
           </header>
           {/* 1st experience */}
           <div className="row">
-            <div className="col-5"></div>
+            <div className="col-5 text-center pt-5">
+              <section>
+                <h4>To be continued ...</h4>
+              </section>
+            </div>
             <div className="col-2">
               <div className={style.line}></div>
             </div>
-            <div className="col-5 text-center">
-              <section>
-                <h5>time</h5>
-                <h4>To be continued</h4>
-                <p>Maybe we can reach the summit together!</p>
-              </section>
+            <div className="col-5 text-center pt-5">
+              <ul>
+                <li className="text-left">
+                  Maybe we can reach the summit together!
+                </li>
+              </ul>
             </div>
           </div>
           {/* 2nd experience */}
           <div className="row">
-            <div className="col-5 text-center">
-              <NavLink to="/education" className={style.nav_link}>
-                <h5>time</h5>
-                <h4>Got Master Degree In Concordia University</h4>
-                <p>Maybe we can reach the summit together!</p>
-              </NavLink>
+            <div className="col-5 text-center pt-5">
+              <section>
+                <h5>05/2019 - 06/2020</h5>
+                <h3>ALIBABA GROUP</h3>
+                <p>Nanjing, China</p>
+                <h5>Front-End Developer (Intern)</h5>
+              </section>
             </div>
             <div className="col-2">
               <div className={style.line}></div>
             </div>
-            <div className="col-5"></div>
+            <div className="col-5 text-center pt-5">
+              <ul>
+                <li className="text-left">
+                  Built reusable UI components in agile environment that display
+                  data and views of backstage management system to assist in
+                  development of Business Advisor project and provided general
+                  application maintenance.
+                </li>
+                <li className="text-left">
+                  Completed Oneness platform project which provides connection
+                  to most company data product interfaces and assists
+                  developers.
+                </li>
+                <li className="text-left">
+                  Adapted to company toolset, quickly and efficiently learning
+                  all unique frameworks and concepts.
+                </li>
+              </ul>
+            </div>
           </div>
           {/* 3rd experience */}
           <div className="row">
-            <div className="col-5"></div>
-            <div className="col-2">
-              <div className={style.line}></div>
-            </div>
-            <div className="col-5 text-center">
+            <div className="col-5 text-center pt-5">
               <section>
-                <h5>time</h5>
-                <h4>ALIBABA GROUP</h4>
-                <h5>Nanjing, China</h5>
-                <p>Maybe we can reach the summit together!</p>
+                <h5>02/2016 - 03/2017</h5>
+                <h3>Thought Works</h3>
+                <p>Shanghai, China</p>
+                <h5>Front-End Developer (Intern)</h5>
               </section>
             </div>
-          </div>
-          {/* 4th experience */}
-          <div className="row">
-            <div className="col-5 text-center">
-              <NavLink to="/education" className={style.nav_link}>
-                <h5>time</h5>
-                <h4>
-                  Got Bachelor Degree In Nanjing University of Science and
-                  Technology
-                </h4>
-                <p>Maybe we can reach the summit together!</p>
-              </NavLink>
-            </div>
             <div className="col-2">
               <div className={style.line}></div>
             </div>
-            <div className="col-5"></div>
-          </div>
-          {/* 5th experience */}
-          <div className="row">
-            <div className="col-5"></div>
-            <div className="col-2">
-              <div className={style.line}></div>
-            </div>
-            <div className="col-5 text-center">
-              <section>
-                <h5>time</h5>
-                <h4>Thought Works</h4>
-                <h5>Shanghai, China</h5>
-                <p>Maybe we can reach the summit together!</p>
-              </section>
+            <div className="col-5 text-center pt-5">
+              <ul>
+                <li className="text-left">
+                  Provided front end development for Community Personnel
+                  Management System, utilizing React and Redux. Built routers
+                  and reusable UI components. Used Redux to store states.
+                </li>
+                <li className="text-left">
+                  Adapted to company toolset, quickly and efficiently learning
+                  all unique frameworks and concepts.
+                </li>
+              </ul>
             </div>
           </div>
         </div>
